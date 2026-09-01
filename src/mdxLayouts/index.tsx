7bc10b386fa2a6f344ba280e4496678d79e72317
ibrahimmahadmamo-coder <ibrahimmahadmamo@gmail.com>
2026-09-02 01:17:11 +0300

    Initialize angelo/ga-config-as-code

---
+++ src/mdxLayouts/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "../components/Link";
+import { FrontMatter } from "../types";
+import { MDXProvider } from "@mdx-js/react";
+import { DocsLayout } from "../layouts/DocsLayout";
+import { CodeBlock } from "../components/CodeBlock";
+import NextImage from "next/image";
+import "twin.macro";
+import {
+  Banner,
+  PriorityBoardingBanner,
+  PublicAPIBanner,
+} from "../components/Banner";
+
+const Image = props => (
+  <a tw="block xl:-mx-8" href={props.src} target="_blank" rel="noopener">
+    <NextImage {...props} />
+  </a>
+);
+
+const components = {
+  a: Link,
+  pre: CodeBlock,
+  Image,
+  Banner,
+  Link,
+  PriorityBoardingBanner,
+  PublicAPIBanner,
+};
+
+const Layout: React.FC<{ frontMatter: FrontMatter }> = props => (
+  <MDXProvider components={components}>
+    <DocsLayout {...props}>{props.children}</DocsLayout>
+  </MDXProvider>
+);
+
+export default Layout;

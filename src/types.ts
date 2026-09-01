bbc869b9211ace100a7797e475394db923250cad
ibrahimmahadmamo-coder <ibrahimmahadmamo@gmail.com>
2026-09-02 01:18:50 +0300

    Initialize docs/tailscale-forwarder-guide

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+import type { IconName } from "@/assets/icons/types";
+
+export interface FrontMatter {
+  title: string;
+  description?: string;
+  url: string;
+  lastModified?: string;
+}
+
+export type ISidebarContent = ISidebarSection[];
+
+export interface IPage {
+  title: string;
+  slug: string;
+  description?: string;
+  category?: string;
+  icon?: IconName;
+  commandName?: string;
+}
+
+export interface IExternalLink {
+  title: string;
+  url: string;
+  icon?: IconName;
+  hideExternalIcon?: boolean;
+}
+
+export interface ISidebarSection {
+  title?: string;
+  slug?: string;
+  content: (IPage | ISubSection | IExternalLink)[];
+  defaultExpanded?: boolean;
+}
+
+export interface ISubSection {
+  subTitle: string | IPage;
+  pages: (IPage | IExternalLink)[];
+}
+
+export namespace Search {
+  interface ResultItem {
+    hierarchies: string[];
+    slug: string;
+    text: string;
+  }
+  export type Result = Record<string, ResultItem[]>;
+  export interface Document {
+    hierarchy_lvl0: string;
+    hierarchy_lvl1: string;
+    hierarchy_lvl2: string;
+    hierarchy_lvl3: string;
+    hierarchy_lvl4: string;
+    url: string;
+    content: string;
+  }
+}

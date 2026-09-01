3fe66412edf47dab98f840a9df6d90a00992a95b
ibrahimmahadmamo-coder <ibrahimmahadmamo@gmail.com>
2026-09-02 01:17:25 +0300

    Initialize zuchka-patch-1

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withContentlayer({
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "user-images.githubusercontent.com",
+      "railway.app",
+      "res.cloudinary.com",
+      "devicons.railway.app",
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/reference/s",
+        destination: "/reference/templates",
+        permanent: true,
+      },
+    ];
+  },
+});
+
+module.exports = nextConfig;
